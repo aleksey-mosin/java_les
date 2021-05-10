b1bf3e4c4fbe0749ed8d3d153d9cae5d9d204fe6
aleksey-mosin <a.mosin@hostiman.ru>
2021-05-10 22:42:23 +0300

    sout(2+2)

--- sandbox/src/main/java/MyProgram3.java
+++ sandbox/src/main/java/MyProgram3.java
@@ -1,5 +1,6 @@
-    public class MyProgram3 {
-public static void main(String[] args) {
-  System.out.println("Hello, Mosin!");
-}
+public class MyProgram3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Mosin!");
+      System.out.println(2+2);
+    }
 }
