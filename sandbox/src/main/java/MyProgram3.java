1feb309ad034182ccc1c9b97949699f256b87c79
aleksey-mosin <a.mosin@hostiman.ru>
2021-05-11 18:43:43 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/MyProgram3.java
+++ sandbox/src/main/java/MyProgram3.java
@@ -1,9 +1,23 @@
 public class MyProgram3 {
+
     public static void main(String[] args) {
-        System.out.println("Hello," + " Aleksey Mosin!");
-      System.out.println(2+2);
-      int l = 9;
-      int s = l * l;
-        System.out.println("Площадь квадрата со стороной " + l + "=" + s);
+        hello("Aleksey Mosin");
+        hello("World");
+        hello("Karina Soikina");
+        double l = 6.6;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со стронами " + a + " и " + b + " = " + area(a, b));
+    }
+    public static void hello(String somebody){
+        System.out.println("Hi, " + somebody + "!");
+    }
+    public static double area(double h){
+        return h * h;
+    }
+    public static double area(double a, double b){
+        return a * b;
     }
 }
