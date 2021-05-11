c20551d05a725881af86c60450698d9e612b3db1
aleksey-mosin <a.mosin@hostiman.ru>
2021-05-11 17:58:20 +0300

    int l + int s = l^2

--- sandbox/src/main/java/MyProgram3.java
+++ sandbox/src/main/java/MyProgram3.java
@@ -1,6 +1,9 @@
 public class MyProgram3 {
     public static void main(String[] args) {
-        System.out.println("Hello," + "Mosin!");
+        System.out.println("Hello," + " Aleksey Mosin!");
       System.out.println(2+2);
+      int l = 9;
+      int s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + "=" + s);
     }
 }
